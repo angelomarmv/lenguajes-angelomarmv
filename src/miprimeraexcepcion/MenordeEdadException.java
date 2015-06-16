121a1f9880c4ce3dd63611ffd3f54ce8548c5bfb
T-107 <T-107@PC259>
2015-06-15 19:45:42 -0500

    EXCEPCIONEESSS

---
+++ src/miprimeraexcepcion/MenordeEdadException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraexcepcion;
+
+
+
+public class MenordeEdadException extends Exception{
+    
+    public MenordeEdadException(){
+    
+   super("eres un chamaco pervertido, no te puedo "+"registrar"); 
+   }
+    
+}
